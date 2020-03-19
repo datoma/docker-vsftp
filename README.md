eee7240cfc775657f743b27f57f0bd8f1dcbcd2d
Michael Hoffmann <github@datoma.de>
2020-03-19 17:39:48 +0100

    changed var names

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 ## Usage
-    docker run --name vsftpd -d -p 21:21 -p 21000-21010:21000-21010 -e FTPUSER="ftpuser|ftppassword" -e FTPSERVERADDRESS=ftp.host.de datoma/vsftpd
+    docker run --name vsftpd -d -p 21:21 -p 21000-21010:21000-21010 -e FTP_USER="ftpuser|ftppassword" -e FTP_SERVER_ADDRESS=ftp.host.de datoma/vsftpd
 
 ## Configuration
 
 Environment variables:
-- `FTPUSER` - space and `|` separated list (optional, default: `ftp|alpineftp`)
+- `FTP_USER` - space and `|` separated list (optional, default: `ftp|alpineftp`)
   - format `name1|password1|[folder1][|uid1] name2|password2|[folder2][|uid2]`
-- `FTPSERVERADDRESS` - external address witch clients can connect passive ports (optional)
-- `MIN_PORT` - minamal port number may be used for passive connections (optional, default `21000`)
-- `MAX_PORT` - maximal port number may be used for passive connections (optional, default `21010`)
+- `FTP_SERVER_ADDRESS` - external address witch clients can connect passive ports (optional)
+- `FTP_MIN_PORT` - minamal port number may be used for passive connections (optional, default `21000`)
+- `FTP_MAX_PORT` - maximal port number may be used for passive connections (optional, default `21010`)
 
 ## USERS examples
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,47 +1,47 @@
 #!/bin/sh
 
 # default user if none is set
-if [ -z "$FTPUSER" ]; then
-  FTPUSER="ftp|alpineftp"
+if [ -z "$FTP_USER" ]; then
+  FTP_USER="ftp|alpineftp"
 fi
 
-for i in $FTPUSER ; do
-    FTPUSERNAME=$(echo $i | cut -d'|' -f1)
-    FTPPASSWORD=$(echo $i | cut -d'|' -f2)
-    FTPFOLDER=$(echo $i | cut -d'|' -f3)
-    UID=$(echo $i | cut -d'|' -f4)
+for i in $FTP_USER ; do
+    FTP_USER_NAME=$(echo $i | cut -d'|' -f1)
+    FTP_PASSWORD=$(echo $i | cut -d'|' -f2)
+    FTP_FOLDER=$(echo $i | cut -d'|' -f3)
+    FTP_UID=$(echo $i | cut -d'|' -f4)
 
-  if [ -z "$FTPFOLDER" ]; then
-    FTPFOLDER="/ftp/$FTPUSERNAME"
+  if [ -z "$FTP_FOLDER" ]; then
+    FTP_FOLDER="/ftp/$FTP_USER_NAME"
   fi
 
-  if [ ! -z "$UID" ]; then
-    UID_OPT="-u $UID"
+  if [ ! -z "$FTP_UID" ]; then
+    FTP_UID_OPT="-u $FTP_UID"
   fi
 
-  echo -e "$FTPPASSWORD\n$FTPPASSWORD" | adduser -h $FTPFOLDER -s /sbin/nologin $UID_OPT $FTPUSERNAME
-  mkdir -p $FTPFOLDER
-  chown $FTPUSERNAME:$FTPUSERNAME $FTPFOLDER
-  unset FTPUSERNAME FTPPASSWORD FTPFOLDER UID
+  echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -h $FTP_FOLDER -s /sbin/nologin $FTP_UID_OPT $FTP_USER_NAME
+  mkdir -p $FTP_FOLDER
+  chown $FTP_USER_NAME:$FTP_USER_NAME $FTP_FOLDER
+  unset FTP_USER_NAME FTP_PASSWORD FTP_FOLDER FTP_UID
 done
 
 
-if [ -z "$MIN_PORT" ]; then
-  MIN_PORT=21000
+if [ -z "$FTP_MIN_PORT" ]; then
+  FTP_MIN_PORT=21000
 fi
 
-if [ -z "$MAX_PORT" ]; then
-  MAX_PORT=21010
+if [ -z "$FTP_MAX_PORT" ]; then
+  FTP_MAX_PORT=21010
 fi
 
-if [ ! -z "$FTPSERVERADDRESS" ]; then
-  ADDR_OPT="-opasv_address=$FTPSERVERADDRESS"
+if [ ! -z "$FTP_SERVER_ADDRESS" ]; then
+  FTP_ADDR_OPT="-opasv_address=$FTP_SERVER_ADDRESS"
 fi
 
 # Used to run custom commands inside container
 if [ ! -z "$1" ]; then
   exec "$@"
 else
-  exec /usr/sbin/vsftpd -opasv_min_port=$MIN_PORT -opasv_max_port=$MAX_PORT $ADDR_OPT /etc/vsftpd/vsftpd.conf
+  exec /usr/sbin/vsftpd -opasv_min_port=$FTP_MIN_PORT -opasv_max_port=$FTP_MAX_PORT $FTP_ADDR_OPT /etc/vsftpd/vsftpd.conf
 fi
 
